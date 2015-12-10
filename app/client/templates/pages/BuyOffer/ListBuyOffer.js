a731e88760f1b92658685691823f2a2b468ec9f5
Aljon Preza <aljonpreza@yahoo.com>
2015-12-09 14:52:00 -1000

    Formatted Date for Buy Offer

--- app/client/templates/pages/BuyOffer/ListBuyOffer.js
+++ app/client/templates/pages/BuyOffer/ListBuyOffer.js
@@ -6,5 +6,11 @@ Template.ListBuyOffer.helpers({
   buyOfferList: function () {
     var currDate = new Date();
     return BuyOffer.find({ "expirationDate": {$gt: currDate} }, {sort: {book: 1, offer: 1}});
+  },
+  formatDate: function (date) {
+    return moment(date).format('ll');
+  },
+  formatTime: function (date) {
+    return moment(date).format('h:mm a');
   }
 });
